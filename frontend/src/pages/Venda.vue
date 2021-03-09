9954e10bd8ea0f585668f564667fcfbd7731303c
Gustavo Bessa <gustavowbessa@gmail.com>
2021-03-09 17:43:09 -0300

    Remoção de console log

--- frontend/src/pages/Venda.vue
+++ frontend/src/pages/Venda.vue
@@ -422,7 +422,6 @@ export default {
             if (data.length > 0) {
               this.table = true
               this.data = data
-              console.log(data.length)
               setTimeout(() => {
                 this.focusedProduto = true
                 this.selectedProduto = []
